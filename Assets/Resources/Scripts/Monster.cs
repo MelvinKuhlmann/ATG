330a65d4216f936e581759b6a4ea93fbc0f6b656
Melvin Kuhlmann <melvin.kuhlmann@gmail.com>
2022-05-27 11:47:32 +0200

    Added zombie animations

---
+++ Assets/Resources/Scripts/Monster.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+namespace Resources.Scripts
+{
+    public abstract class Monster : MonoBehaviour
+    {
+        protected abstract Animator getAnimator();
+        
+        protected void ChangeAnimationTo(string anim)
+        {
+            var animator = getAnimator();
+            
+            foreach (var animatorControllerParameter in animator.parameters)
+            {
+                animator.SetBool(animatorControllerParameter.name, false);
+            }
+            animator.SetBool(anim, true);
+        }
+        
+        protected void Die()
+        {
+            Destroy(gameObject);
+        }
+    }
+}
--- Assets/Resources/Scripts/Zombie.cs
+++ Assets/Resources/Scripts/Zombie.cs
@@ -1,20 +1,25 @@
+using System;
 using System.Collections;
+using UnityEditor.Rendering;
 using UnityEngine;
 
 namespace Resources.Scripts
 {
-    public class Zombie : MonoBehaviour
+    public class Zombie : Monster
     {
         private Transform _playerTransform;
         private const float Speed = 2F;
+        private ZombieState _zombieState;
+        public Animator animator;
 
         // Start is called before the first frame update
         private void Start()
         {
             _playerTransform = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
+            _zombieState = ZombieState.Walking;
         }
 
-        private void Update()
+        private void FixedUpdate()
         {
             var ownPosition = transform.position;
             var playerPosition = _playerTransform.position;
@@ -30,6 +35,58 @@ namespace Resources.Scripts
 
             transform.position =
                 Vector2.MoveTowards(ownPosition, playerPosition, Speed * Time.deltaTime);
+
+            if (Vector2.Distance(ownPosition, playerPosition) <= 2F)
+            {
+                _zombieState = ZombieState.Attacking;
+            }
+            else
+            {
+                _zombieState = ZombieState.Walking;
+            }
+        }
+
+        private void Update()
+        {
+            HandleState();
+        }
+
+        private void OnCollisionEnter2D(Collision2D col)
+        {
+            if (col.gameObject.tag.Equals("Bullet"))
+            {
+                _zombieState = ZombieState.Dying;
+            }
+        }
+
+        private void HandleState()
+        {
+            switch (_zombieState)
+            {
+                case ZombieState.Walking:
+                    ChangeAnimationTo("isWalking");
+                    break;
+                case ZombieState.Attacking:
+                    ChangeAnimationTo("isAttacking");
+                    break;
+                case ZombieState.Dying:
+                    ChangeAnimationTo("isDying");
+                    break;
+                default:
+                    throw new ArgumentOutOfRangeException();
+            }
+        }
+
+        private enum ZombieState
+        {
+            Walking,
+            Attacking,
+            Dying
+        }
+
+        protected override Animator getAnimator()
+        {
+            return animator;
         }
     }
 }
