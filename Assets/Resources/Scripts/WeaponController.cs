4011017ea07dada2c5abc9de9eb177b2eed94e1f
Melvin Kuhlmann <melvin.kuhlmann@gmail.com>
2022-05-29 01:21:26 +0200

    Added HUD with Ammo on it

--- Assets/Resources/Scripts/WeaponController.cs
+++ Assets/Resources/Scripts/WeaponController.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using UnityEngine.Events;
 
 namespace Resources.Scripts
 {
@@ -6,13 +7,24 @@ namespace Resources.Scripts
     {
         public GameObject bullet;
         public Transform firePoint;
+        private int _amountOfAmmo;
+        public UnityEvent<int> updateAmmo = new();
 
+        // Start is called before the first frame update
+        private void Start()
+        {
+            _amountOfAmmo = 10;
+            updateAmmo.Invoke(_amountOfAmmo);
+        }
+        
         // Update is called once per frame
         private void Update()
         {
-            if (Input.GetMouseButtonDown(0))
+            if (Input.GetMouseButtonDown(0) && _amountOfAmmo >= 1)
             {
                 Instantiate(bullet, firePoint.position, transform.rotation);
+                _amountOfAmmo -= 1;
+                updateAmmo.Invoke(_amountOfAmmo);
             }
         }
     }
