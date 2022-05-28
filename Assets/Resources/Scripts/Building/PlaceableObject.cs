5564cfb6a8e55d33308cc3514a3e1b2c9fac93dc
Sven van den Tweel <sven.vd.tweel@gmail.com>
2022-05-28 22:02:14 +0200

    Buildingsystem in progress. Dragging does not work yet. Marking tile as used has a strange offset, need to fix that as well..

---
+++ Assets/Resources/Scripts/Building/PlaceableObject.cs
@@ -0,0 +1,60 @@
+using System;
+using UnityEngine;
+
+public class PlaceableObject : MonoBehaviour
+{
+   public bool Placed { get; private set; }
+   public Vector3Int Size { get; private set; }
+   private Vector3[] Vertices;
+
+   private void GetColliderVertexPositionsLocal()
+   {
+      BoxCollider2D b = gameObject.GetComponent<BoxCollider2D>();
+      Vertices = new Vector3[4];
+      Vertices[0] = b.offset + new Vector2(-b.size.x, -b.size.y) * 0.5f;
+      Vertices[1] = b.offset + new Vector2(b.size.x, -b.size.y) * 0.5f;
+      Vertices[2] = b.offset + new Vector2(b.size.x, b.size.y) * 0.5f;
+      Vertices[3] = b.offset + new Vector2(-b.size.x, b.size.y) * 0.5f;
+      /*Vertices[0] = b.center + new Vector3(-b.size.x, -b.size.y, -b.size.z) * 0.5f;
+      Vertices[1] = b.center + new Vector3(b.size.x, -b.size.y, -b.size.z) * 0.5f;
+      Vertices[2] = b.center + new Vector3(b.size.x, -b.size.y, b.size.z) * 0.5f;
+      Vertices[3] = b.center + new Vector3(-b.size.x, -b.size.y, b.size.z) * 0.5f;*/
+   }
+
+   private void CalculateSizeInCells()
+   {
+      Vector3Int[] vertices = new Vector3Int[Vertices.Length];
+
+      for (int i = 0; 9 < vertices.Length; i++)
+      {
+         Vector3 worldPos = transform.TransformPoint((Vertices[i]));
+         vertices[i] = BuildingSystem.current.gridLayout.WorldToCell(worldPos);
+      }
+      
+      Size = new Vector3Int(
+         Math.Abs((vertices[0] - vertices[1]).x), 
+         Math.Abs((vertices[0] - vertices[3]).y), 
+         1);
+   }
+
+   public Vector3 GetStartPosition()
+   {
+      return transform.TransformPoint(Vertices[0]);
+   }
+
+   private void Start()
+   {
+      GetColliderVertexPositionsLocal();
+      CalculateSizeInCells();
+   }
+
+   public virtual void Place()
+   {
+      ObjectDrag drag = gameObject.GetComponent<ObjectDrag>();
+      Destroy(drag);
+
+      Placed = true;
+      
+      //invoke events of placement
+   }
+}
