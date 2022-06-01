f7c70a62427cf59d768d8a1812785b476ace1792
Melvin Kuhlmann <melvin.kuhlmann@gmail.com>
2022-06-02 01:29:11 +0200

    Added gunshot sound and muzzle flash

--- Assets/Resources/Scripts/WeaponController.cs
+++ Assets/Resources/Scripts/WeaponController.cs
@@ -5,16 +5,21 @@ namespace Resources.Scripts
 {
     public class WeaponController : MonoBehaviour
     {
+        private int _amountOfAmmo;
+        
         public GameObject bullet;
         public Transform firePoint;
-        private int _amountOfAmmo;
+        public GameObject muzzleFlash;
         public UnityEvent<int> updateAmmo = new();
+        public AudioClip gunShotClip;
 
         // Start is called before the first frame update
         private void Start()
         {
             _amountOfAmmo = 10;
             updateAmmo.Invoke(_amountOfAmmo);
+            
+            InvokeRepeating("GarbageCollectAudioSources", 0F, 5F);
         }
         
         // Update is called once per frame
@@ -22,9 +27,36 @@ namespace Resources.Scripts
         {
             if (Input.GetMouseButtonDown(0) && _amountOfAmmo >= 1)
             {
+                PlayGunShot();
+                
+                muzzleFlash.SetActive(true);
+                Invoke("HideFlash", 0.1F);
                 Instantiate(bullet, firePoint.position, transform.rotation);
                 _amountOfAmmo -= 1;
                 updateAmmo.Invoke(_amountOfAmmo);
+            }
+        }
+
+        private void PlayGunShot()
+        {
+            var audioSource = gameObject.AddComponent<AudioSource>();
+            audioSource.clip = gunShotClip;
+            audioSource.Play();
+        }
+
+        private void HideFlash()
+        {
+            muzzleFlash.SetActive(false);
+        }
+
+        private void GarbageCollectAudioSources()
+        {
+            foreach (var audioSource in gameObject.GetComponents<AudioSource>())
+            {
+                if (!audioSource.isPlaying)
+                {
+                    Destroy(audioSource);
+                }
             }
         }
     }
